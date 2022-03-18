251c053ee7b190885ca771776e93ba84249914f7
BALA <52425674+Batman-C137@users.noreply.github.com>
2022-03-18 10:49:13 +0530

    Add files via upload

---
+++ OnlineFlightTicketBookingSystem-FleetModule/src/main/java/com/capg/sringboot/OnlineFlightTicketBookingSystemFleetModuleApplication.java
@@ -0,0 +1,13 @@
+package com.capg.sringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFlightTicketBookingSystemFleetModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFlightTicketBookingSystemFleetModuleApplication.class, args);
+	}
+
+}
---
+++ OnlineFlightTicketBookingSystem-FleetModule/src/main/java/com/capg/sringboot/entity/Fleet.java
@@ -0,0 +1,73 @@
+package com.capg.sringboot.entity;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name="fleet")
+public class Fleet {
+			@Id
+			private int fleetId;
+			private String code;
+			private String model;
+			private int totalEconomySeats;
+			private int totalPremiumSeats;
+			private int totalBusinessSeats;
+			
+			
+			public Fleet() {
+				super();
+				// TODO Auto-generated constructor stub
+			}
+			public Fleet(int fleetId, String code, String model, int totalEconomySeats, int totalPremiumSeats,
+					int totalBusinessSeats) {
+				super();
+				this.fleetId = fleetId;
+				this.code = code;
+				this.model = model;
+				this.totalEconomySeats = totalEconomySeats;
+				this.totalPremiumSeats = totalPremiumSeats;
+				this.totalBusinessSeats = totalBusinessSeats;
+			}
+			public int getFleetId() {
+				return fleetId;
+			}
+			public void setFleetId(int fleetId) {
+				this.fleetId = fleetId;
+			}
+			public String getCode() {
+				return code;
+			}
+			public void setCode(String code) {
+				this.code = code;
+			}
+			public String getModel() {
+				return model;
+			}
+			public void setModel(String model) {
+				this.model = model;
+			}
+			public int getTotalEconomySeats() {
+				return totalEconomySeats;
+			}
+			public void setTotalEconomySeats(int totalEconomySeats) {
+				this.totalEconomySeats = totalEconomySeats;
+			}
+			public int getTotalPremiumSeats() {
+				return totalPremiumSeats;
+			}
+			public void setTotalPremiumSeats(int totalPremiumSeats) {
+				this.totalPremiumSeats = totalPremiumSeats;
+			}
+			public int getTotalBusinessSeats() {
+				return totalBusinessSeats;
+			}
+			public void setTotalBusinessSeats(int totalBusinessSeats) {
+				this.totalBusinessSeats = totalBusinessSeats;
+			}
+			
+			
+			
+}
